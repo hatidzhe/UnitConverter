6767870f066eef8a3d7414d2b15664f57c8d32b2
Hatidzhe <yusufova.h@gmail.com>
2015-05-23 14:07:47 +0300

    chg: new program inserted

--- src/com/hatidzhe/Main.java
+++ src/com/hatidzhe/Main.java
@@ -3,6 +3,13 @@ package com.hatidzhe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
+       System.out.println("Wie viele kcal sind umzuwandeln?");
+       String input = new java.util.Scanner( System.in ).nextLine();
+       double value = Double.parseDouble(input);
+       double result = value * 1.163;
+       String result1 = Double.toString( result );
+       System.out.println( input + "kcal sind " + result1 +"Wh");
+   
     }
 }
