343fd9310bb0f4aba41e52f1efd6db06f8cd4e6e
Hatidzhe <yusufova.h@gmail.com>
2015-05-23 17:39:49 +0300

    chg: function added

--- src/com/hatidzhe/Converter.java
+++ src/com/hatidzhe/Converter.java
@@ -4,4 +4,7 @@ package com.hatidzhe;
  * Created by hatidzhe on 15-5-22.
  */
 public class Converter {
+    void askForCalories() {
+        System.out.println("Wie viele kcal sind umzuwandeln?");
+    }
 }
