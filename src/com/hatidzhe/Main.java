973c143dc8883d17d8cc0ac25cd48422fe4bb8c5
Samuel Albrecht <elsamuko@gmail.com>
2015-05-23 13:50:30 +0200

    chg: add comments

--- src/com/hatidzhe/Main.java
+++ src/com/hatidzhe/Main.java
@@ -3,13 +3,20 @@ package com.hatidzhe;
 public class Main {
 
     public static void main(String[] args) {
-	
-       System.out.println("Wie viele kcal sind umzuwandeln?");
-       String input = new java.util.Scanner( System.in ).nextLine();
-       double value = Double.parseDouble(input);
-       double result = value * 1.163;
-       String result1 = Double.toString( result );
-       System.out.println( input + "kcal sind " + result1 +"Wh");
+
+        // askForCalories()
+        System.out.println("Wie viele kcal sind umzuwandeln?");
+
+        // getInput()
+        String input = new java.util.Scanner( System.in ).nextLine();
+
+        // doConversion()
+        double value = Double.parseDouble(input);
+        double result = value * 1.163;
+        String result1 = Double.toString( result );
+
+        // answer()
+        System.out.println( input + "kcal sind " + result1 +"Wh");
    
     }
 }
