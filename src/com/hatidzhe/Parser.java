44782fad576ca580d75443eca9cc7c2fe4ef5926
Hatidzhe <yusufova.h@gmail.com>
2015-05-27 17:58:06 +0300

    chg:4.class added

---
+++ src/com/hatidzhe/Parser.java
@@ -0,0 +1,11 @@
+package com.hatidzhe;
+
+/**
+ * Created by hatidzhe on 15-5-24.
+ */
+public class Parser {
+    public static double transformString(String input) {
+        double input1 = Double.parseDouble(input);
+        return input1;
+    }
+}
