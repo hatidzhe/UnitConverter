0fd74cec853ab900547f483f333d34b5a1c6756c
Hatidzhe <yusufova.h@gmail.com>
2015-05-27 18:15:59 +0300

    chg: 5. class added

---
+++ src/com/hatidzhe/Serializer.java
@@ -0,0 +1,11 @@
+package com.hatidzhe;
+
+/**
+ * Created by hatidzhe on 15-5-27.
+ */
+public class Serializer {
+    public static String transformDouble(double output1){
+        String output = Double.toString(output1);
+        return output;
+    }
+}
