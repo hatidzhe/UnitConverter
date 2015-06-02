82964c248efcab00641e490e9982827a3521778d
Hatidzhe <yusufova.h@gmail.com>
2015-06-02 14:45:25 +0300

    chg: exception added

--- src/com/hatidzhe/Parser.java
+++ src/com/hatidzhe/Parser.java
@@ -3,9 +3,18 @@ package com.hatidzhe;
 /**
  * Created by hatidzhe on 15-5-24.
  */
-public class Parser {
+      public class Parser {
+    String input;
+    String ex;
+
+
     public static double transformString(String input) {
-        double input1 = Double.parseDouble(input);
-        return input1;
+
+        try {
+            double input1 = Double.parseDouble(input);
+            return input1;
+        } catch (NumberFormatException ex) {
+            return 0 ;
+        }
     }
 }
